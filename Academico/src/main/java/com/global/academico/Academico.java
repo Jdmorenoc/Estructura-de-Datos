e5aa8357438faded78076d396eb07d2c48f0544e
Juan Diego Moreno Contreras <jdmorenoc@ufpso.edu.co>
2024-08-16 07:34:00 -0500

    Contenido

---
+++ Academico/src/main/java/com/global/academico/Academico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.global.academico;
+
+/**
+ *
+ * @author SCIS2-02
+ */
+public class Academico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
