8f5ae8fa40ee136f235515f79d2332ef2a56c210
Juan Diego Moreno Contreras <jdmorenoc@ufpso.edu.co>
2024-09-03 07:52:29 -0500

    subiendo procesos

---
+++ ListaSimple/src/main/java/global/co/listasimple/Nodo.java
@@ -0,0 +1,44 @@
+package global.co.listasimple;
+
+public class Nodo {
+   private String nombre;
+   private int edad;
+   private float promedio;
+   
+   private Nodo enlace;
+
+    public Nodo getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(Nodo enlace) {
+        this.enlace = enlace;
+    }
+   
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+    
+    
+}
