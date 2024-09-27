7e1c4da78c06ec2c0e74cc8af6b4305503f4eafd
Juan Diego Moreno Contreras <jdmorenoc@ufpso.edu.co>
2024-09-27 07:43:04 -0500

    Subiendo doble

---
+++ doble/src/main/java/global/doble/NodoDoble.java
@@ -0,0 +1,35 @@
+package global.doble;
+
+public class NodoDoble {
+   
+    private NodoDoble anterior;
+    private NodoDoble siguiente;
+    private String dato;
+
+    public NodoDoble getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoDoble anterior) {
+        this.anterior = anterior;
+    }
+
+    public NodoDoble getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoDoble siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+    
+    
+    
+}
